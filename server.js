8882808513d9e70f315b3367353c697b305f1ab9
yordanis <yordanis@gmail.com>
2021-07-26 16:44:30 -0400

    Mongoose Conectado

--- server.js
+++ server.js
@@ -418,7 +418,7 @@ app.use(function (req, res) {
   }
 });
 
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 8080, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
